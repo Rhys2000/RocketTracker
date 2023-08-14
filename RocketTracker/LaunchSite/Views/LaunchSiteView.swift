892a97e5fc0188a5e04f16bea0814b2fd3aa47b8
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-14 07:53:27 -0700

    Added foregroundColor to both of these files to be able to differentiate between them while testing TabView

---
+++ RocketTracker/LaunchSite/Views/LaunchSiteView.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchSiteView.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 8/14/23.
+//
+
+import SwiftUI
+
+struct LaunchSiteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(Color.red)
+    }
+}
+
+struct LaunchSiteView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchSiteView()
+    }
+}
