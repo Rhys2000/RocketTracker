6c4dcb2a1b2dfcd68b80eb87ef31db70a5ff9611
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-14 07:25:29 -0700

    Initial Commit

---
+++ RocketTracker/RocketTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketTrackerApp.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RocketTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
