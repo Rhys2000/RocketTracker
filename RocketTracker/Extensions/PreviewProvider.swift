439505d7359acbbffc3058a5f8c0fbf8dffa6023
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-15 15:27:36 -0700

    Added an extension to PreviewProvider to be able to create a dummy Launch to test component of the LaunchView

---
+++ RocketTracker/Extensions/PreviewProvider.swift
@@ -0,0 +1,23 @@
+//
+//  PreviewProvider.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 8/15/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension PreviewProvider {
+    static var dev: DevelopmentPreview {
+        return DevelopmentPreview.instance
+    }
+}
+
+class DevelopmentPreview {
+    static let instance = DevelopmentPreview()
+    
+    private init() { }
+    
+    let launch = Launch(id: "2020-101", missionName: "National Reconnaisance Office Launch - 108", alternativeMissionName: "USA-321 / USA-313", abbreviatedMissionName: "NROL-108", liftOffTime: "2020-12-19T14:00:00+0000", orbitalDestination: .leo, launchProvider: .spacex, launchSite: "Kennedy", launchPad: "LC-39A", vehicleName: "Falcon 9", vehicleVariant: "Block 5", missionStatus: .failure, crewedLaunch: false, staticFire: false, staticFireToLaunchWindow: nil, boosterID: ["B1059-5"], boosterRecoveryAttempted: [true], boosterRecoveryMethod: [.rtls], boosterRecoveryLocation: ["LZ-1"], boosterRecoveryDistance: [14.75], boosterRecoveryOutcome: [.success], numberOfFairingFlights: [1, 1], fairingRecoveryAttempted: [true, true], fairingRecoveryMethod: [.rtls, .rtls], fairingRecoveryLocation: ["Atlantic", "Atlantic"], fairingRecoveryDistance: [328], fairingRecoveryOutcome: [.success, .success], supportVessel: ["Go Ms. Tree", "Go Searcher"], supportVesselRoles: [[.dsv], [.dsv]], description: ["SpaceX is targeting Thursday, December 17 for the launch of the NROL-108 mission, which will launch from Launch Complex 39A (LC-39A) at Kennedy Space Center, Florida."], livesteamLink: "https://www.youtube.com/watch?v=9OeVwaFBkfE")
+}
