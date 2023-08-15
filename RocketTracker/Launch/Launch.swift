43c2c0ddf4e8deb40f6b2ff4fa4bfe1256e7ac69
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-15 15:09:31 -0700

    Created first iteration of the Launch struct which can hold a lot of information about a single Launch. Also created surrounding enums that have relevance to Launch

--- RocketTracker/Launch/Launch.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Launch.swift
-//  RocketTracker
-//
-//  Created by Rhys Julian-Jones on 8/14/23.
-//
-
-import Foundation
---
+++ RocketTracker/Launch/Model/Launch.swift
@@ -0,0 +1,66 @@
+//
+//  Launch.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 8/14/23.
+//
+
+import Foundation
+
+struct Launch: Identifiable, Codable {
+    let id: String
+    let missionName: String
+    let alternativeMissionName: String?
+    let abbreviatedMissionName: String?
+    let liftOffTime: String
+    let orbitalDestination: OrbitDestination
+    let launchProvider: LaunchProvider
+    let launchSite: String?
+    let launchPad: String?
+    let vehicleName: String
+    let vehicleVariant: String
+    let missionStatus: MissionOutcome
+    let staticFire: Bool?
+    let staticFireToLaunchWindow: Int?
+    let boosterID: [String?]
+    let boosterRecoveryAttempted: [Bool?]
+    let boosterRecoveryMethod: [RecoveryMethod?]
+    let boosterRecoveryLocation: [String?]
+    let boosterRecoveryDistance: [Double?]
+    let boosterRecoveryOutcome: [RecoveryOutcome?]
+    let numberOfFairingFlights: [Int?]
+    let fairingRecoveryAttempted: [Bool?]
+    let fairingRecoveryMethod: [RecoveryMethod?]
+    let fairingRecoveryLocation: [String?]
+    let fairingRecoveryDistance: [Double?]
+    let fairingRecoveryOutcome: [RecoveryOutcome?]
+    let supportVessel: [String?]
+    let supportVesselRoles: [[VesselRole?]]
+    let description: [String?]
+    let livesteamLink: String?
+}
+
+enum OrbitDestination: String, Codable {
+    case leo = "LEO"
+}
+
+enum LaunchProvider: String, Codable {
+    case spacex = "SpaceX"
+}
+
+enum RecoveryMethod: String, Codable {
+    case rtls = "RTLS"
+}
+
+enum RecoveryOutcome: String, Codable {
+    case success = "Success"
+}
+
+enum MissionOutcome: String, Codable {
+    case failure = "Failure"
+}
+
+enum VesselRole: String, Codable {
+    case dsv = "DSV"
+}
+
