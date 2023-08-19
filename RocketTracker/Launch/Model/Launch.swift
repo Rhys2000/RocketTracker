28d07bc406d4559ae6a100db3dde1a2d02289a34
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-18 19:44:05 -0700

    Changed all variables to no longer be optional. Adjusted the names of some enums that could be consolidated, added additional cases to all enums

--- RocketTracker/Launch/Model/Launch.swift
+++ RocketTracker/Launch/Model/Launch.swift
@@ -11,44 +11,44 @@ import Foundation
  JSON Format
  {
      "id" : "",
-     "mission_name" : "",
-     "alternative_mission_name" : "",
-     "abbreviated_mission_name" : "",
-     "lift_off_time" : "",
-     "orbital_destination" : "",
-     "launch_provider" : "",
-     "launch_site_name" : "",
-     "launch_site_pad" : "",
-     "vehicle_name" : "",
-     "vehicle_variant" : "",
-     "mission_outcome" : "",
-     "crewed_launch" : ,
-     "static_fire" : ,
-     "static_fire_to_launch_window" : ,
-     "booster_name" : [""],
-     "booster_recovery_attempted" : [],
-     "booster_recovery_method" : [""],
-     "booster_recovery_location" : [""],
-     "booster_recovery_distance" : [],
-     "booster_recovery_outcome" : [""],
-     "number_of_fairing_flights" : [],
-     "fairing_recovery_attempted" : [],
-     "fairing_recovery_method" : [""],
-     "fairing_recovery_location" : [""],
-     "fairing_recovery_distance" : [],
-     "fairing_recovery_outcome" : [""],
-     "support_vessel" : [""],
-     "support_vessel_roles" : [[""]],
+     "missionName" : "",
+     "alternativeMissionName" : "",
+     "abbreviatedMissionName" : "",
+     "liftOffTime" : "YYYY-MM-DDThh:mm:ss+0000",
+     "orbitalDestination" : "",
+     "launchProvider" : "SpaceX",
+     "launchSiteName" : "",
+     "launchSitePad" : "",
+     "vehicleName" : "Falcon 9",
+     "vehicleVariant" : "Block ",
+     "missionOutcome" : "",
+     "crewedLaunch" : ,
+     "staticFire" : ,
+     "staticFireToLaunchWindow" : ,
+     "boosterName" : [""],
+     "boosterRecoveryAttempted" : [],
+     "boosterRecoveryMethod" : [""],
+     "boosterRecoveryLocation" : [""],
+     "boosterRecoveryDistance" : [],
+     "boosterRecoveryOutcome" : [""],
+     "numberOfFairingFlights" : [],
+     "fairingRecoveryAttempted" : [],
+     "fairingRecoveryMethod" : [""],
+     "fairingRecoveryLocation" : [""],
+     "fairingRecoveryDistance" : [],
+     "fairingRecoveryOutcome" : [""],
+     "supportVessel" : [""],
+     "supportVesselRole" : [[""]],
      "description" : [""],
-     "livestream_link" : "",
- }
+     "livestreamLink" : "",
+ },
  */
 
 struct Launch: Identifiable, Codable {
     let id: String
     let missionName: String
-    let alternativeMissionName: String?
-    let abbreviatedMissionName: String?
+    let alternativeMissionName: String
+    let abbreviatedMissionName: String
     let liftOffTime: String
     let orbitalDestination: OrbitDestination
     let launchProvider: LaunchProvider
@@ -56,31 +56,40 @@ struct Launch: Identifiable, Codable {
     let launchSitePad: String
     let vehicleName: String
     let vehicleVariant: String
-    let missionOutcome: MissionOutcome
+    let missionOutcome: Outcome
     
-    let crewedLaunch: Bool?
-    let staticFire: Bool?
-    let staticFireToLaunchWindow: Int?
-    let boosterName: [String?]
+    let crewedLaunch: Bool
+    let staticFire: Bool
+    let staticFireToLaunchWindow: Int
+    let boosterName: [String]
     let boosterRecoveryAttempted: [Bool]
     let boosterRecoveryMethod: [RecoveryMethod?]
-    let boosterRecoveryLocation: [String?]
-    let boosterRecoveryDistance: [Double?]
-    let boosterRecoveryOutcome: [RecoveryOutcome?]
-    let numberOfFairingFlights: [Int?]
+    let boosterRecoveryLocation: [String]
+    let boosterRecoveryDistance: [Double]
+    let boosterRecoveryOutcome: [Outcome]
+    let numberOfFairingFlights: [Int]
     let fairingRecoveryAttempted: [Bool]
-    let fairingRecoveryMethod: [RecoveryMethod?]
-    let fairingRecoveryLocation: [String?]
-    let fairingRecoveryDistance: [Double?]
-    let fairingRecoveryOutcome: [RecoveryOutcome?]
-    let supportVessel: [String?]
-    let supportVesselRole: [[VesselRole?]]
+    let fairingRecoveryMethod: [RecoveryMethod]
+    let fairingRecoveryLocation: [String]
+    let fairingRecoveryDistance: [Double]
+    let fairingRecoveryOutcome: [Outcome]
+    let supportVessel: [String]
+    let supportVesselRole: [[VesselRole]]
     let description: [String?]
     let livesteamLink: String?
 }
 
 enum OrbitDestination: String, Codable {
-    case leo = "LEO"
+    case leo = "LEO" //Low Earth Orbit
+    case pleo = "PLEO" //Polar Low Earth Orbit
+    case meo = "MEO" //Medium Earth Orbit
+    case geo = "GEO" //Geostationary Equatorial Orbit
+    case gto = "GTO" //Geostationary Transfer Orbit
+    case iss = "ISS" //International Space Station
+    case sso = "SSO" //Sun-Synchronous Orbit
+    case heo = "HEO" //High Earth Orbit
+    case sub = "SUB" //Suborbital Trajectory
+    case tli = "TLI" //Trans-Lunar Injection Orbit
 }
 
 enum LaunchProvider: String, Codable {
@@ -88,18 +97,21 @@ enum LaunchProvider: String, Codable {
 }
 
 enum RecoveryMethod: String, Codable {
-    case rtls = "RTLS"
+    case expended = "Expended"
+    case parachute = "Parachute"
+    case splashdown = "Splashdown"
+    case returnToLaunchSite = "RTLS"
+    case droneship = "Droneship"
+    case netCatch = "Catch"
+    case notAvailable = "NA"
 }
 
-enum RecoveryOutcome: String, Codable {
+enum Outcome: String, Codable {
     case success = "Success"
-}
-
-enum MissionOutcome: String, Codable {
+    case partialSuccess = "Partial Success"
     case failure = "Failure"
-}
-
-enum VesselRole: String, Codable {
-    case dsv = "DSV"
+    case prelaunchExplosion = "Prelaunch Explosion"
+    case unknown = "Unknown"
+    case notAvailable = "NA"
 }
 
