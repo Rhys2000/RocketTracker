49d52474e9331846d56a82b494075157a9be75e9
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-24 11:36:19 -0700

    Added the ability to switch between past and future launches. Very rough state. Needs major rework

--- RocketTracker/Launch/Views/LaunchView.swift
+++ RocketTracker/Launch/Views/LaunchView.swift
@@ -10,20 +10,24 @@ import SwiftUI
 struct LaunchView: View {
     
     @EnvironmentObject private var lvm: LaunchViewModel
+    @State private var showFutureLaunches: Bool = false
     
     var body: some View {
         ZStack {
             Color.theme.primaryBackground
                 .ignoresSafeArea()
             VStack {
-                List {
-                    ForEach(lvm.allLaunches) { launch in
-                        LaunchRowView(launch: launch)
-                            .listRowBackground(Color.theme.primaryBackground)
-                            .listRowSeparator(.hidden)
-                    }
+                Rectangle()
+                    .frame(width: UIScreen.main.bounds.width, height: 10)
+                listBar
+                    .padding(.horizontal)
+                if(!showFutureLaunches) {
+                    futureLaunchList
+                        .transition(.move(edge: .leading))
+                } else {
+                    pastLaunchList
+                        .transition(.move(edge: .trailing))
                 }
-                .listStyle(PlainListStyle())
                 Spacer(minLength: 0)
             }
         }
@@ -34,5 +38,58 @@ struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
         LaunchView()
             .environmentObject(dev.launchVM)
+    }
+}
+
+extension LaunchView {
+    private var listBar: some View {
+        HStack {
+            Text("Future")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.primaryText)
+                .onTapGesture {
+                    showFutureLaunches.toggle()
+                }
+                .frame(width: UIScreen.main.bounds.width / 2, height: 40)
+            Spacer()
+            Rectangle()
+                .frame(width: 1, height: 40)
+                .background(Color.black)
+            Spacer()
+            Text("Past")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.primaryText)
+                .onTapGesture {
+                    showFutureLaunches.toggle()
+                }
+                .frame(width: UIScreen.main.bounds.width / 2)
+        }
+        .padding(.horizontal)
+        .background(Color.theme.primaryBackground)
+        .shadow(radius: 10)
+    }
+    
+    private var pastLaunchList: some View {
+        List {
+            ForEach(lvm.pastLaunches) { launch in
+                LaunchRowView(launch: launch)
+                    .listRowBackground(Color.theme.primaryBackground)
+                    .listRowSeparator(.hidden)
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var futureLaunchList: some View {
+        List {
+            ForEach(lvm.futureLaunches) { launch in
+                LaunchRowView(launch: launch)
+                    .listRowBackground(Color.theme.primaryBackground)
+                    .listRowSeparator(.hidden)
+            }
+        }
+        .listStyle(PlainListStyle())
     }
 }
