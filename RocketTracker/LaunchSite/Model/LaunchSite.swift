91a081bdec6af0669207ae53569fe996a5e4003c
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-25 12:15:04 -0700

    Added two structs LaunchSite and LaunchPad. A LaunchSite can have multiple LaunchPads. Launch Sites are places like Kennedy Space Center, a site that hosts numerous launch pads

---
+++ RocketTracker/LaunchSite/Model/LaunchSite.swift
@@ -0,0 +1,60 @@
+//
+//  LaunchSite.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 8/14/23.
+//
+
+import Foundation
+import MapKit
+
+/*
+ JSON Format
+ {
+     "shortName" : "",
+     "fullName" : "",
+     "abbrName" : "",
+     "previousNames" : [""],
+     "latitude" : ,
+     "longitude" : ,
+     "territory" : "",
+     "country" : "",
+     "launchPads" : [
+         {
+              "shortName" : "",
+              "fullName" : "",
+              "latitude" : ,
+              "longitude" : ,
+         }
+     ]
+ },
+ */
+
+struct LaunchSite: Identifiable, Codable {
+    
+    let shortName: String
+    let fullName: String
+    let abbrName: String
+    let previousNames: [String]
+    let latitude: Double
+    let longitude: Double
+    let territory: String
+    let country: String
+    let launchPads: [LaunchPad]
+    
+    var id: String {
+        shortName
+    }
+}
+
+struct LaunchPad: Identifiable, Codable {
+    
+    let shortName: String
+    let fullName: String
+    let latitude: Double
+    let longitude: Double
+    
+    var id: String {
+        shortName + String(latitude)
+    }
+}
