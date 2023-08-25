6c44df4103d94cbc5608e3cf57cd1ddae1b08d91
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-25 09:01:55 -0700

    Changed launchProvider to no longer be an enum and instead be a String. Updated DeveloperPreview instance of a launch

--- RocketTracker/Launch/Model/Launch.swift
+++ RocketTracker/Launch/Model/Launch.swift
@@ -14,26 +14,26 @@ import Foundation
      "missionName" : "",
      "altMissionName" : "",
      "abbrMissionName" : "",
-     "liftOffTime" : "YYYY-MM-DDT00:00:00+0000",
+     "liftOffTime" : "YYYY-MM-DDThh:mm:ss+0000",
      "orbitalDestination" : "",
-     "launchProvider" : "SpaceX",
+     "launchProvider" : "",
      "launchSiteName" : "",
      "launchSitePad" : "",
-     "vehicleName" : "Falcon 9",
-     "vehicleVariant" : "Block 5",
-     "missionOutcome" : "Upcoming",
-     "crewedLaunch" : "false",
+     "vehicleName" : "",
+     "vehicleVariant" : "",
+     "missionOutcome" : "",
+     "crewedLaunch" : "",
      "staticFireGap" : "",
-     "boosterName" : ["B"],
+     "boosterName" : [""],
      "boosterRecoveryMethod" : [""],
      "boosterRecoveryLocation" : [""],
      "boosterRecoveryDistance" : [""],
-     "boosterRecoveryOutcome" : ["Upcoming"],
+     "boosterRecoveryOutcome" : [""],
      "numberOfFairingFlights" : ["", ""],
      "fairingRecoveryMethod" : ["", ""],
      "fairingRecoveryLocation" : ["", ""],
      "fairingRecoveryDistance" : "",
-     "fairingRecoveryOutcome" : ["Upcoming", "Upcoming"],
+     "fairingRecoveryOutcome" : ["", ""],
      "supportVessel" : [""],
      "supportVesselRole" : [[""]],
      "payloadDescription" : [""],
@@ -48,7 +48,7 @@ struct Launch: Identifiable, Codable {
     let abbrMissionName: String
     let liftOffTime: String
     let orbitalDestination: OrbitDestination
-    let launchProvider: LaunchProvider
+    let launchProvider: String
     let launchSite: String
     let launchSitePad: String
     let vehicleName: String
@@ -105,10 +105,6 @@ enum OrbitDestination: String, Codable {
     case sel2 = "SEL2" //Sun-Earth Lagrange Point 2
     case helio = "HELIO" //Heliocentric Orbit
     case notAvailable = "NA"
-}
-
-enum LaunchProvider: String, Codable {
-    case spacex = "SpaceX"
 }
 
 enum RecoveryMethod: String, Codable {
