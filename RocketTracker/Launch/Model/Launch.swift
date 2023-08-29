60e0c382196bc78bb0e707b984479b85d137989e
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-29 15:44:59 -0700

    Added getFullName function to the OrbitDestination enum that returns a string with the full name of the case instead of the abbreviation used in the json data

--- RocketTracker/Launch/Model/Launch.swift
+++ RocketTracker/Launch/Model/Launch.swift
@@ -109,6 +109,43 @@ enum OrbitDestination: String, Codable {
     case sel2 = "SEL2" //Sun-Earth Lagrange Point 2
     case helio = "HELIO" //Heliocentric Orbit
     case notAvailable = "NA"
+    
+    func getFullName() -> String {
+        switch self {
+        case .leo:
+            return "Low Earth Orbit"
+        case .pleo:
+            return "Polar Low Earth Orbit"
+        case .plro:
+            return "Polar Orbit"
+        case .meo:
+            return "Medium Earth Orbit"
+        case .geo:
+            return "Geostationary Equatorial Orbit"
+        case .gto:
+            return "Geostationary Transfer Orbit"
+        case .iss:
+            return "International Space Station"
+        case .sso:
+            return "Sun Synchronous Orbit"
+        case .heo:
+            return "High Earth Orbit"
+        case .helo:
+            return "Highly Elliptical Orbit"
+        case .sub:
+            return "Suborbital Trajectory"
+        case .tli:
+            return "Trans-Lunar Injection Orbit"
+        case .sel1:
+            return "Sun-Earth Lagrange Point 1"
+        case .sel2:
+            return "Sun-Earth Lagrange Point 2"
+        case .helio:
+            return "Heliocentric Orbit"
+        case .notAvailable:
+            return "Not Available"
+        }
+    }
 }
 
 enum RecoveryMethod: String, Codable {
