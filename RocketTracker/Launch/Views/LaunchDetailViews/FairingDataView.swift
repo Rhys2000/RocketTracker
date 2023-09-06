8d6d17e1976f8d2d9313b4ab7e7842541225cd71
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-06 11:47:55 -0700

    Added another case to the getRecoveryMethod function for the fairings in a a launch

--- RocketTracker/Launch/Views/LaunchDetailViews/FairingDataView.swift
+++ RocketTracker/Launch/Views/LaunchDetailViews/FairingDataView.swift
@@ -65,11 +65,17 @@ extension FairingDataView {
             else if(firstFairing.method == .netCatch && secondFairing.method == .netCatch && firstFairing.outcome == .failure && secondFairing.outcome == .failure) {
                 return "Both fairing halves failed to be caught by \(firstFairing.location) and \(secondFairing.location) \(firstFairing.distance) km downrange and could not be recovered"
             }
+            else if(firstFairing.method == .netCatch && secondFairing.method == .splashdown && firstFairing.outcome == .success && secondFairing.outcome == .success) {
+                return "One fairing half was successfully caught by \(firstFairing.location) and the other half successfully splashed down and was recovered \(secondFairing.distance) km downrange in the \(secondFairing.location) ocean"
+            }
             else if(firstFairing.method == .netCatch && secondFairing.method == .splashdown && firstFairing.outcome == .partialSuccess && secondFairing.outcome == .success) {
                 return "One fairing half was attempted to be caught by \(firstFairing.location) but was recovered from the water and the other half successfully splashed down and was recovered \(secondFairing.distance) km downrange in the \(secondFairing.location) ocean"
             }
             else if(firstFairing.method == .netCatch && secondFairing.method == .splashdown && firstFairing.outcome == .failure && secondFairing.outcome == .success) {
                 return "One fairing half failed to be caught by \(firstFairing.location) and could not be recovered while the other half successfully splashed down and was recovered \(secondFairing.distance) km downrange in the \(secondFairing.location) ocean"
+            }
+            else if(firstFairing.method == .netCatch && secondFairing.method == .netCatch && firstFairing.outcome == .aborted && secondFairing.outcome == .aborted) {
+                return "Both fairings halves were meant to be caught by \(firstFairing.location) and \(secondFairing.location) \(firstFairing.distance) km downrange but the recovery attempt was aborted"
             }
             
             else if(firstFairing.method == .splashdown && secondFairing.method == .splashdown && firstFairing.outcome == .success && secondFairing.outcome == .success) {
