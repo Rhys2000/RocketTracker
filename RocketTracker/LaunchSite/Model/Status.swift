72c9894b5fde28d14945061f8099071d2d190e51
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-06 15:47:48 -0700

    Added the Status enum which will be used in several different structs: LaunchSite, Vessel, LaunchPad, Vehicle, Variant, Satellite, and Capsule

---
+++ RocketTracker/LaunchSite/Model/Status.swift
@@ -0,0 +1,14 @@
+//
+//  Status.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 9/6/23.
+//
+
+import Foundation
+
+enum Status: String, Codable {
+    case active = "Active"
+    case retired = "Retired"
+    case repair = "Repair"
+}
