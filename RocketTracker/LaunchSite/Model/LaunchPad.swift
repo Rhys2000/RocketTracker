438009428a8495efa5c5e5dbeb019d62bb9b7b41
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-08 10:55:42 -0700

    Changed status to use the enum OperationalStatus instead of a String. Added a function that converts doubles for latitude and longitude into coordinates instead

--- RocketTracker/LaunchSite/Model/LaunchPad.swift
+++ RocketTracker/LaunchSite/Model/LaunchPad.swift
@@ -12,7 +12,7 @@ struct LaunchPad: Identifiable, Codable {
     let shortName: String
     let fullName: String
     let operatorName: String
-    let status: String
+    let status: OperationalStatus
     let latitude: Double
     let longitude: Double
     
@@ -20,5 +20,25 @@ struct LaunchPad: Identifiable, Codable {
     
     func getLaunchPadFullName() -> String {
         return "\(fullName) (\(shortName))"
+    }
+    
+    func convertToNWCoordinates(latitude: Double, longitude: Double) -> String {
+        func degreesMinutesSeconds(coordinate: Double, isLatitude: Bool) -> (Int, Int, Double, String) {
+            let direction = isLatitude ? (coordinate >= 0 ? "N" : "S") : (coordinate >= 0 ? "E" : "W")
+            let absoluteCoordinate = abs(coordinate)
+            let degrees = Int(absoluteCoordinate)
+            let remainingMinutes = (absoluteCoordinate - Double(degrees)) * 60
+            let minutes = Int(remainingMinutes)
+            let seconds = (remainingMinutes - Double(minutes)) * 60
+            return (degrees, minutes, seconds, direction)
+        }
+
+        let (latDegrees, latMinutes, latSeconds, latDirection) = degreesMinutesSeconds(coordinate: latitude, isLatitude: true)
+        let (lonDegrees, lonMinutes, lonSeconds, lonDirection) = degreesMinutesSeconds(coordinate: longitude, isLatitude: false)
+
+        let latitudeCoordinate = "\(latDegrees)° \(latMinutes)' \(String(format: "%.0f", latSeconds))\" \(latDirection)"
+        let longitudeCoordinate = "\(lonDegrees)° \(lonMinutes)' \(String(format: "%.0f", lonSeconds))\" \(lonDirection)"
+
+        return "\(latitudeCoordinate), \(longitudeCoordinate)"
     }
 }
