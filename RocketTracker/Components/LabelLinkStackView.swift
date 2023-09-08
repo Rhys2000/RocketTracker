3bffff9c7a16bb8f30454b78e1bc03868ab58bd4
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-08 16:06:44 -0700

    Added LabelLinkStack View which create a Label like in LabelDataStackView but instead of taking in data it takes in a URL and when the second Text object is clicked takes the user to that link

---
+++ RocketTracker/Components/LabelLinkStackView.swift
@@ -0,0 +1,40 @@
+//
+//  LabelLinkStackView.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 9/8/23.
+//
+
+import SwiftUI
+
+struct LabelLinkStackView: View {
+    
+    let labelName: String
+    let url: URL
+    let pastLaunch: Bool
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Text(labelName)
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            Text(pastLaunch ? "Watch Again" : "Watch Live")
+                .underline()
+                .foregroundColor(Color.theme.accent)
+                .font(.title3)
+                .bold()
+                .onTapGesture {
+                    if UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            Spacer()
+        }
+    }
+}
+
+struct LabelLinkStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        LabelLinkStackView(labelName: "Livestream:", url: URL(string: dev.launch.livestreamLink)!, pastLaunch: dev.launch.pastLaunch)
+    }
+}
