b2ba198a94029c4d97036f70dc54db107d53d63f
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-11 16:40:22 -0700

    Added a couple new stored properties to VehicleVariant.json and remained another

--- RocketTracker/Vehicle/Models/VehicleVariant.swift
+++ RocketTracker/Vehicle/Models/VehicleVariant.swift
@@ -10,13 +10,14 @@ import Foundation
 struct VehicleVariant: Identifiable, Codable {
     var id = UUID()
     let variantName: String
-    let height: String
+    let vehicleHeight: String
+    let vehicleDiameter: String
     let fairingHeight: String
     let fairingDiameter: String
     let manufacturer: String
     let numberOfStages: String
     let strapOnBoosters: String
     let priceTag: String
-    let thrustAtLiftOff: String
+    let thrustAtLiftOff: String //in kN
     let status: OperationalStatus
 }
