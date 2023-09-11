7a556e8f652756c715e831474e9bae1ec00531fa
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-11 16:02:23 -0700

    Added VehicleView which will be the main view for the Vehicle tab

---
+++ RocketTracker/Vehicle/Views/VehicleView.swift
@@ -0,0 +1,20 @@
+//
+//  VehicleView.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 9/8/23.
+//
+
+import SwiftUI
+
+struct VehicleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VehicleView_Previews: PreviewProvider {
+    static var previews: some View {
+        VehicleView()
+    }
+}
