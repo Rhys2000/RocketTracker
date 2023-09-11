2b3f6ffa7dd55a07a02571edcbc59e2d7f8a386d
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-11 14:06:32 -0700

    Added some basic stored properties to Vehicle, as well as an array of Variants for said Vehicle. Also added computed properties for numberOfSuccessfulMissions, numerOfPartiallySuccessfulMissions, numberOfFailedMissions, successRate, successStreak, and totalMissionFlown.

--- RocketTracker/Vehicle/Models/Vehicle.swift
+++ RocketTracker/Vehicle/Models/Vehicle.swift
@@ -9,6 +9,63 @@ import Foundation
 
 struct Vehicle: Identifiable, Codable {
     let vehicleName: String
+    let description: [String]
+    let vehicleVariants: [VehicleVariant]
     
     var id: String { vehicleName }
+    
+    var missionsFlown: Double {
+        var temp: Double = 0
+        for launch in LaunchDataService().previousLaunches {
+            if(launch.vehicleName == vehicleName) {
+                temp += 1
+            }
+        }
+        return temp
+    }
+    
+    var successfulMissions: Double {
+        var temp: Double = 0
+        for launch in LaunchDataService().previousLaunches {
+            if(launch.vehicleName == vehicleName && launch.missionOutcome == .success) {
+                temp += 1
+            }
+        }
+        return temp
+    }
+    
+    var partiallySuccessfulMissions: Double {
+        var temp: Double = 0
+        for launch in LaunchDataService().previousLaunches {
+            if(launch.vehicleName == vehicleName && launch.missionOutcome == .partialSuccess) {
+                temp += 1
+            }
+        }
+        return temp
+    }
+    
+    var failedMissions: Double {
+        var temp: Double = 0
+        for launch in LaunchDataService().previousLaunches {
+            if(launch.vehicleName == vehicleName && (launch.missionOutcome == .failure || launch.missionOutcome == .explosion)) {
+                temp += 1
+            }
+        }
+        return temp
+    }
+    
+    var successRate: Double {
+        return (successfulMissions + (partiallySuccessfulMissions * 0.5)) / missionsFlown
+    }
+    
+    var successStreak: Double {
+        var temp: Double = 0
+        for launch in LaunchDataService().previousLaunches {
+            if(launch.vehicleName == vehicleName && launch.missionOutcome == .success) {
+                temp += 1
+            }
+        }
+        print(temp)
+        return temp
+    }
 }
