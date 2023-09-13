d73941f919aace16a702d7c67038d93ef2b84553
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-12 17:32:32 -0700

    Created the LabelDataView file

---
+++ RocketTracker/Components/LabelDataView.swift
@@ -0,0 +1,20 @@
+//
+//  LabelDataView.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 9/12/23.
+//
+
+import SwiftUI
+
+struct LabelDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LabelDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        LabelDataView()
+    }
+}
