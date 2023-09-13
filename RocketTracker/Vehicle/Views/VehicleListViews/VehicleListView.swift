fd892cf14ca359f38caaa5fc92d4d33067ddbd40
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-12 17:07:19 -0700

    Added VehicleListView to the list of files

---
+++ RocketTracker/Vehicle/Views/VehicleListViews/VehicleListView.swift
@@ -0,0 +1,20 @@
+//
+//  VehicleListView.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 9/12/23.
+//
+
+import SwiftUI
+
+struct VehicleListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VehicleListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VehicleListView()
+    }
+}
