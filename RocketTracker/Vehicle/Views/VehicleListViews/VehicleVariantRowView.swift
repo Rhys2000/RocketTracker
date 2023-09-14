6afe186ccfe8fda1ca85a59162ee365de6367d95
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-09-13 21:12:41 -0700

    Created the file for VehicleVariantRowView

---
+++ RocketTracker/Vehicle/Views/VehicleListViews/VehicleVariantRowView.swift
@@ -0,0 +1,20 @@
+//
+//  VehicleVariantRowView.swift
+//  RocketTracker
+//
+//  Created by Rhys Julian-Jones on 9/13/23.
+//
+
+import SwiftUI
+
+struct VehicleVariantRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VehicleVariantRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        VehicleVariantRowView()
+    }
+}
